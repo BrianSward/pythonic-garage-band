f63bcfc70e347f8050dac59ec5fb69408a070adb
Brian Sward <bsward@gmail.com>
2022-12-22 16:47:27 -0800

    guitar/drums tests passed

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,13 +1,34 @@
 class Band:
-    pass
+    def __init__(self, name):
+        self.name = name
 
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        pass
+
+    def play_solos(self, member):
+        pass
+
+    def to_list(self):
+        pass
 
 class Musician:
-    pass
+    def __init__(self, name):
+        self.name = name
 
 
 class Guitarist(Musician):
-    pass
+    def __init__(self, name, instrument="guitar"):
+        self.name = name
+        self.instrument = instrument
+
+    def __str__(self):
+        return f"My name is {self.name} and I play {self.instrument}"
+
+    def __repr__(self):
+        return f"Guitarist instance. Name = {self.name}"
 
 
 class Bassist(Musician):
@@ -15,4 +36,12 @@ class Bassist(Musician):
 
 
 class Drummer(Musician):
-    pass
+    def __init__(self, name, instrument="drums"):
+        self.name = name
+        self.instrument = instrument
+
+    def __str__(self):
+        return f"My name is {self.name} and I play {self.instrument}"
+
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
